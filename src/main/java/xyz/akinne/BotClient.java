46036b7600f8d86f1cddb5022ec4d83abf40fd3b
xAkinne <akinnefall@gmail.com>
2023-06-22 23:43:53 +0200

    Start

---
+++ src/main/java/xyz/akinne/BotClient.java
@@ -0,0 +1,40 @@
+package xyz.akinne;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
+import net.dv8tion.jda.api.sharding.ShardManager;
+
+import javax.security.auth.login.LoginException;
+
+
+public class BotClient {
+
+    private final Dotenv config;
+    private final ShardManager shardManager;
+
+    public BotClient(){
+        config = Dotenv.configure().load();
+        String token = config.get("TOKEN");
+
+        DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault(token);
+        builder.setStatus(OnlineStatus.ONLINE);
+        builder.setActivity(Activity.listening("Kururin~"));
+        shardManager = builder.build();
+    }
+
+    public Dotenv getConfig(){
+        return config;
+    }
+
+    public ShardManager getShardManager(){
+        return shardManager;
+    }
+
+    public static void main(String[] args) {
+        BotClient bot = new BotClient();
+    }
+
+
+}
